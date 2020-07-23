9a9fad2e8851236d2910f396d456889a685b826d
hayashi <touyalin@gmail.com>
2020-07-23 12:24:30 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # 首頁特化的寫法
   root "pages#index"
-
   # get "/", to: "pages#index"
+  
+  get "/about", to: "pages#about"
 end
