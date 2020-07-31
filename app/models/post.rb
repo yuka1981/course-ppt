66a6fd7b24325a6922fdd35d8783b021c3e9b681
hayashi <touyalin@gmail.com>
2020-07-31 12:31:58 +0800

    install hirb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,26 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :serial, uniqueness: true
+  # validates :serial, uniqueness: true, allow_nil: true
 
-  before_save :create_serial
+  # model callback life cycle: 新增跟更新
+  # save -> 
+  # valid -> 
+  # before_validation -> 
+  # validate -> 
+  # after_validation -> 
+  # before_save -> 
+  # (before_create -> create -> after_create [update 不會走這段]) -> 
+  # after_save -> 
+  # after_commit
+
+  before_create :create_serial
 
   private
   # self.serial 指的是 post model 中的 serial 欄位
   def create_serial
     self.serial = serial_generator(10)
+    # 原型 self.serial=(serial_generator(10))
   end
 
   def serial_generator(n)
